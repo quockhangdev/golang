ca4c2db1edf4ec453ca901cebcec290c98a8ac1a
Quoc-Khang Tran <twkan2001@gmail.com>
2023-12-05 13:12:17 +0700

    Basic server

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello World!"))
+	if err != nil {
+		return
+	}
+}
 
 func main() {
-	fmt.Print("Hello World")
+	addr := os.Getenv("ADDR")
+
+	if len(addr) == 0 {
+		addr = ":8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", HelloHandler)
+
+	log.Printf("Server is listening at %s...", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
